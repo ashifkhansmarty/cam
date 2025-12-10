ca90512adb92a66e2447a79e30b2117c8dfaffc2
ashifkhansmarty <77468662+ashifkhansmarty@users.noreply.github.com>
2025-12-10 07:05:44 +0530

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  telegramToken: process.env.TELEGRAM_BOT_TOKEN,
-  telegramChatId: process.env.TELEGRAM_CHAT_ID,
+  telegramToken: process.env.8377935849:AAGJfPkstm8yNLRvapk_dU1SBPuSLPm0bUc,
+  telegramChatId: process.env.8230629068,
   captureInterval: 3000
 };
